c81b8d4eff46e5dcb66b632f8a8d6ca8ea9e7fb4
wangonya <kwangonya@gmail.com>
2019-04-14 12:06:06 +0300

    solve day 2/30 days of code - operators

---
+++ python-proficiency/operators.py
@@ -0,0 +1,16 @@
+# https://www.hackerrank.com/challenges/30-operators/problem
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost*tip_percent/100
+    tax = meal_cost*tax_percent/100
+    total = meal_cost+tip+tax
+    print(round(total))
+
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    solve(meal_cost, tip_percent, tax_percent)
