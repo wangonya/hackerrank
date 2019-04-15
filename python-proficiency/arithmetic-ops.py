685488e35238d0c86d44f5d5a15bb2a34c3b4be8
wangonya <kwangonya@gmail.com>
2019-04-15 14:26:39 +0300

    solve new challenges

---
+++ python-proficiency/arithmetic-ops.py
@@ -0,0 +1,13 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+
+def solution(a, b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    solution(a, b)
---
+++ python-proficiency/comprehensions.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+
+
+def solution(x, y, z, n):
+    comprehension = []
+    pass
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+    solution(x, y, z, n)
