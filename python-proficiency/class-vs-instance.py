b4bd49bee2e1c473cf91efab702784a51f6b0231
wangonya <kwangonya@gmail.com>
2019-04-16 13:34:51 +0300

    solve day 4 - classes vs instance

---
+++ python-proficiency/class-vs-instance.py
@@ -0,0 +1,23 @@
+# https://www.hackerrank.com/challenges/30-class-vs-instance/problem
+
+
+class Person:
+    def __init__(self, initialAge):
+        # run some checks on initialAge
+        self.age = initialAge
+        if self.age < 0:
+            print("Age is not valid, setting age to 0.")
+            self.age = 0
+
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if self.age in range(13):
+            print("You are young.")
+        elif self.age in range(13, 18):
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
+    def yearPasses(self):
+        # Increment the age of the person in here
+        self.age += 1
