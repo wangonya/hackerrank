6bb499776263d0378a35ebf71da1dddafbb18742
wangonya <kwangonya@gmail.com>
2019-04-29 14:13:45 +0300

    solve more exceptions

---
+++ more-exceptions.py
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/30-more-exceptions/problem
+
+
+class Calculator:
+    @staticmethod
+    def power(n, p):
+        if n | p < 0:
+            raise ValueError('n and p should be non-negative')
+        return n**p
+
+
+myCalculator = Calculator()
+T = int(input())
+for i in range(T):
+    n, p = map(int, input().split())
+    try:
+        ans = myCalculator.power(n, p)
+        print(ans)
+    except Exception as e:
+        print(e)
