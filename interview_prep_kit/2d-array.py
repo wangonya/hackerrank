3e04355a4cad997218ed03b9b7671a0d948f18ca
wangonya <kwangonya@gmail.com>
2020-02-19 20:12:21 +0300

    add solution

--- interview_prep_kit/2d-array.py
+++ interview_prep_kit/2d-array.py
@@ -3,4 +3,10 @@ arr = []
 for _ in range(6):
     arr.append(list(map(int, input().rstrip().split())))
 
-print(arr)
+sums = []
+
+for i in range(4):
+    for j in range(4):
+        sums.append(arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2])
+
+print(max(sums))
